baaffcf5935d06fc8c107497c9bc0b7869aa9252
recceleas <mbreneiser@sa01.rc.fas.harvard.edu>
2015-08-06 14:19:12 -0400

    update

--- iggybase/mod_auth/forms.py
+++ iggybase/mod_auth/forms.py
@@ -1,11 +1,10 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length
-
+from wtforms.widgets import Input
 
 class LoginForm( Form ):
     username = StringField( 'Username', validators = [ DataRequired( ), Length( 1, 16 ) ] )
     password = PasswordField( 'Password', validators = [ DataRequired( ) ] )
     remember_me = BooleanField( 'Remember me' )
-    submit = SubmitField( 'Login' )
-    register = SubmitField( 'Register' )
+    login = SubmitField( 'Login' )
