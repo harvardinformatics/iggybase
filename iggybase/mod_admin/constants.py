09ea552ed85939cbe3f592aee21b27cc18791a92
recceleas <mbreneiser@g.harvard.edu>
2016-01-19 16:29:32 -0500

    db merge

--- iggybase/mod_admin/constants.py
+++ iggybase/mod_admin/constants.py
@@ -3,3 +3,18 @@ Menu Constants
 """
 MENU_NAVBAR_ROOT = 'AdminNavBar'
 MENU_SIDEBAR_ROOT = 'AdminSideBar'
+
+ADMIN = 1
+COREUSER = 2
+USER = 3
+READONLY = 4
+LABADMIN = 5
+COREMANAGER = 6
+ROLE = {
+    ADMIN: 'Admin',
+    COREUSER: 'CoreUser',
+    USER: 'User',
+    READONLY: 'ReadOnly',
+    LABADMIN: 'LabAdmin',
+    COREMANAGER: 'CoreManager'
+}
