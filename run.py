da94ee6122eedcd25f4bf8ad5d43ea87dea67278
Reuven Koblick <reuven@g.harvard.edu>
2016-02-04 15:21:59 -0500

    Duplicate all Role based models.

--- run.py
+++ run.py
@@ -2,11 +2,13 @@ import os
 import socket
 from iggybase import create_app
 
-rootdir = os.path.basename( os.path.dirname( os.path.abspath( __file__ ) ) )
-hostname = socket.gethostname()
-config_name = hostname + '.' + rootdir
+def make_app():
+    rootdir = os.path.basename( os.path.dirname( os.path.abspath( __file__ ) ) )
+    hostname = socket.gethostname()
+    config_name = hostname + '.' + rootdir
+    return create_app( config_name )
 
-iggybase = create_app( config_name )
+iggybase = make_app()
 
 if __name__ == '__main__':
     iggybase.run( )
