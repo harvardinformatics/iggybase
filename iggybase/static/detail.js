47cd4ee0281f84dd0b482a94e16f8810f5534ff4
portermahoney <portermahoney@gmail.com>
2016-02-26 11:00:27 -0500

    fix links to data entry from detail

--- iggybase/static/detail.js
+++ iggybase/static/detail.js
@@ -7,12 +7,9 @@ $( document ).ready( function () {
 ( function( $ ) {
     $.fn.openDataEntry = function () {
         var hidden_fields = $("#hidden_fields");
-        var url = '';
-        var url_root = $URL_ROOT;
-        if (url_root) {
-            url += url_root
-        }
-        url += '/' + hidden_fields.find('input[name=mod]').val()
+        var url = $URL_ROOT;
+        url += hidden_fields.find('input[name=facility]').val()
+            + '/' + hidden_fields.find('input[name=mod]').val()
             + '/data_entry/' + hidden_fields.find('input[name=table]').val()
             + '/' + hidden_fields.find('input[name=row_name]').val()
         window.location = (url);
