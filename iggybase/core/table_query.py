2be0ef74054d3074b119581f47ba7c9c7ca9bf32
portermahoney <portermahoney@gmail.com>
2016-03-28 12:39:24 -0400

    all detail links to core module

--- iggybase/core/table_query.py
+++ iggybase/core/table_query.py
@@ -107,7 +107,7 @@ class TableQuery:
                 self.table_dict.append(row_dict)
 
     def get_link(self, url_root, page = None, table = None):
-        link = url_root + g.facility + '/' + g.module
+        link = url_root + g.facility + '/' + 'core'
         if page:
             link += '/' + page
         if table:
