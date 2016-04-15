c7a7d76304babb54b90d3f39afcc89e3d7abe0d6
Marc Breneiser <marc.breneiser@gmail.com>
2016-04-15 14:45:08 -0400

    handle unlogged user

--- iggybase/base_routes.py
+++ iggybase/base_routes.py
@@ -1,5 +1,6 @@
 import iggybase.templating as templating
-from flask import g, redirect, url_for
+from flask import g, redirect, url_for, abort, redirect
+from flask_security.core import current_user
 
 def index():
     return templating.render_template( 'index.html' )
@@ -11,16 +12,22 @@ def message(page_temp, page_msg):
     return templating.page_template(page_temp, page_msg=page_msg)
 
 def forbidden():
-    return templating.page_template('forbidden')
+    if not current_user.is_authenticated:
+        return redirect(url_for('security.login'))
+    else:
+        return templating.page_template('forbidden')
 
 def page_not_found():
-    return templating.page_template('not_authorized')
+    if not current_user.is_authenticated:
+        return redirect(url_for('security.login'))
+    else:
+        return templating.page_template('not_authorized')
 
 def home():
     """redirects to user home page
     """
-    if not g.user:
-        abort( 403 )
+    if not current_user.is_authenticated:
+        redirect(url_for('security.login'))
     if g.user.home_page:
         home_page = g.user.home_page
     else:
