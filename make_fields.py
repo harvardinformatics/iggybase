4dbc82b49eb4162a7fca000e6d8606ff3132fcb6
Reuven Koblick <reuven@g.harvard.edu>
2016-04-21 15:34:13 -0400

    Works with errors. Cannot create a field for organization_id. Also need to look into NoResultFound exceptions. Very unsatisfying.

--- make_fields.py
+++ make_fields.py
@@ -1,3 +1,4 @@
+import sys
 from sqlalchemy import inspect
 from sqlalchemy.sql.functions import func
 from iggybase.database import db_session as sess
@@ -18,22 +19,20 @@ def input_model():
         if mod in mod_chk:
             return mod
         else:
-            print('%s is not an admin Model')
+            print('%s is not an admin Model' % repr(mod))
 
     
 mod_name = input_model()
 table_object_id = sess.query(TableObject.id). \
     filter_by(name=eval(mod_name).__tablename__).one()[0]
 
-id_nbr = None
-try:
-    id_nbr = int(sess.query(func.max(eval(mod_name).name)).one()[0][1:]) + 1
-except ValueError:
-    print('Cannot create a name number, using the col.name instead')
+id_nbr = int(sess.query(func.max(Field.name)).one()[0][1:]) + 1
 
 insp = inspect(eval(mod_name))
 for aa, col in insp.columns.items():
+
     field = Field(active=True,
+                  name='F%06d' % (id_nbr),
                   field_name=col.name,
                   table_object_id=table_object_id,
                   primary_key=col.primary_key,
@@ -41,14 +40,32 @@ for aa, col in insp.columns.items():
                   order=1,
                   description=col.name,
                   data_type_id=type_map[col.type.__visit_name__][0])
+
     length = getattr(col.type, 'length', None)
     if length:
         field.length = length
-    field.name = 'F%06d' % (id_nbr) if id_nbr else col.name
+        
+
+
+    if col.foreign_keys:
+        fk = col.foreign_keys.pop()   ## only one foreign key.
+        table, fld = fk.target_fullname.split('.')
+        tobj = sess.query(TableObject).filter_by(name=table).one()
+        field.foreign_key_table_object_id = getattr(tobj, fld, None)
+
+        # In order to find the matching foreign_key_field_id, we need to
+        # create a map in core because the sql driver chokes when looking for an
+        # equivalent name in the field variable. So, a little trickiness
+        fobj = sess.query(Field).filter_by(table_object_id=tobj.id). \
+                filter_by(field_name='id').one()
+        field.foreign_key_field_id = fobj.id
+            
     sess.add(field)
     try:
         sess.commit()
     except:
-        print('error')
+        print('Failed to create field for %s' % col.name)
+
+    id_nbr += 1
 
 
