adbc59fa09060224dc294c368d28b5c6d7056af8
portermahoney <portermahoney@gmail.com>
2016-06-29 16:17:02 -0400

    add pdf download endpoint

--- iggybase/billing/routes.py
+++ iggybase/billing/routes.py
@@ -4,20 +4,31 @@ from . import billing
 from iggybase import core, templating
 from iggybase.billing.invoice_collection import InvoiceCollection
 from iggybase.decorators import templated
+from flask_weasyprint import render_pdf, HTML
 
 @billing.route('/')
 @templated()
 def default():
     return templating.page_template_context('index.html')
 
-@billing.route('/invoice/<year>/<month>', defaults={'page_context': 'base-context'})
-@billing.route( '/invoice/<year>/<month>/<page_context>' )
+@billing.route( '/invoice/<year>/<month>' )
 @login_required
-def invoice(facility_name, year, month, page_context):
+def invoice(facility_name, year, month):
     template = 'invoice_generator'
     print('invoice gen')
     ic = InvoiceCollection(int(year), int(month))
     return render_template('invoice_base.html',
             module_name = 'billing',
             invoice = ic.invoices[84])
+
+@billing.route( '/invoice/<year>/<month>/download' )
+@login_required
+def invoice_download(facility_name, year, month):
+    template = 'invoice_generator'
+    print('invoice gen')
+    ic = InvoiceCollection(int(year), int(month))
+    html = render_template('invoice_base.html',
+            module_name = 'billing',
+            invoice = ic.invoices[84])
+    return render_pdf(HTML(string=html))
 
