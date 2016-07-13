e5a2d735ff80b459aef1e8cdc8913c65bda5ca70
portermahoney <portermahoney@gmail.com>
2016-07-13 11:22:56 -0400

    fix edit functionality on review invoice page

--- iggybase/static/review.js
+++ iggybase/static/review.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
         if (names.length > 0) {
             var url = $URL_ROOT;
             url += hidden_fields.find('input[name=facility]').val()
-                + '/' + hidden_fields.find('input[name=mod]').val()
+                + '/core'
                 + '/multiple_entry/' + table_name
                 + '/' + JSON.stringify(names);
             if ( url.length > 2000 )
