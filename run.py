198dd15ffc8b692f858c90496c8de16d98f9ee08
portermahoney <portermahoney@gmail.com>
2016-10-27 09:31:26 -0400

    create_app no longer takes config name so remove from run

--- run.py
+++ run.py
@@ -5,8 +5,7 @@ from iggybase import create_app
 def make_app():
     rootdir = os.path.basename( os.path.dirname( os.path.abspath( __file__ ) ) )
     hostname = socket.gethostname()
-    config_name = hostname + '.' + rootdir
-    return create_app( config_name )
+    return create_app()
 
 iggybase = make_app()
 
