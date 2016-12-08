22352d7408a25334f39e472134d2f87755b56ce3
recceleas <mbreneiser@g.harvard.edu>
2016-12-08 15:25:33 -0500

    extended tables

--- iggybase/models.py
+++ iggybase/models.py
@@ -8,6 +8,7 @@ tables = table_factory.table_objects()
 for table_object in tables:
     if table_object.TableObject.name not in Base.metadata.tables:
         class_name = TableFactory.to_camel_case(table_object.TableObject.name)
+        logging.info('class_name: ' + class_name)
         if hasattr(table_object, 'Extension'):
             new_table = table_factory.table_object_factory(class_name, table_object.TableObject,
                                                            table_object.Extension)
