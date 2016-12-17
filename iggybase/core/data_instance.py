80d8b60f93b7ab881338ae81a08c2630afb48b9f
recceleas <mbreneiser@g.harvard.edu>
2016-12-17 10:23:32 -0500

    tables

--- iggybase/core/data_instance.py
+++ iggybase/core/data_instance.py
@@ -271,13 +271,14 @@ class DataInstance:
 
         if self.fields[table_name].fields[table_name + "|" + field_name].is_foreign_key:
             field_value = self.set_foreign_key_field_id(table_name, field_name, field_value)
+            logging.info(table_name + "|" + field_name + ' field_value: ' + str(field_value))
 
-        if table_name != 'history' and field_name not in exclude_list and \
+        if (table_name != 'history' and field_name not in exclude_list and
                 ((getattr(self.instances[table_name][instance_name]['instance'], field_name) is None and
-                  field_value is not None) or
-                 field_value != getattr(self.instances[table_name][instance_name]['instance'], field_name)) and \
+                          field_value is not None) or
+                         field_value != getattr(self.instances[table_name][instance_name]['instance'], field_name)) and
                 not (field_name == 'name' and field_value is None and
-                     'new' in self.instances[table_name][instance_name]['instance'].name):
+                     'new' in self.instances[table_name][instance_name]['instance'].name)):
             self.instances[table_name][instance_name]['save'] = True
             new_key = self.add_new_instance('history', 'new')
 
