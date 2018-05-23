54ffdc5ff32112821eac7759f83a2aa95c8fd565
portermahoney <portermahoney@gmail.com>
2018-05-23 09:38:46 -0400

    make clone possible from detail page

--- iggybase/static/detail.js
+++ iggybase/static/detail.js
@@ -1,6 +1,7 @@
 $( document ).ready( function () {
     $("#data_entry").click(function(){$.fn.openDataEntry();});
     $('.summary_table').DataTable();
+    $("#clone").click(function(){$.fn.clone();});
 } );
 
 
@@ -16,3 +17,25 @@ $( document ).ready( function () {
     }
 } ) ( jQuery );
 
+( function( $ ) {
+    $.fn.clone = function () {
+        var tbl = $("#detail_table tr");
+        var detail = {}
+        for (var i=0;i<tbl.length;i++) {
+            var tds = $(tbl[i]).find('td');
+            var key = 'data_entry_' + tds[0].innerText.toLowerCase().replace(' ', '_') + '_1';
+            detail[key] = tds[1].innerText;
+        }
+        console.log(detail);
+        var detail_json = JSON.stringify(detail);
+        console.log(detail_json);
+        var hidden_fields = $("#hidden_fields");
+        var url = $URL_ROOT;
+        url += hidden_fields.find('input[name=facility]').val()
+            + '/' + hidden_fields.find('input[name=mod]').val()
+            + '/data_entry/' + hidden_fields.find('input[name=table]').val()
+            + '/new?data=' + detail_json;
+        window.location = (url);
+
+    }
+} ) ( jQuery );
